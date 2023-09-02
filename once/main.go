dbbfb2cce56764273ec399b402042ddb8f4ef36e
rusisg <rasul.nulrybekov@gmail.com>
2023-09-02 16:27:01 +0600

    monkey

---
+++ once/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+)
+
+var missionCompleted bool
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(100)
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			if foundTreasure() {
+				markMissionCompleted()
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+	checkMissionCompletion()
+}
+
+func checkMissionCompletion() {
+	if missionCompleted {
+		fmt.Println("Mission is completed")
+	} else {
+		fmt.Println("Mission is failure")
+	}
+}
+
+func markMissionCompleted() {
+	missionCompleted = true
+	fmt.Println("Mission is now completed")
+}
+
+func foundTreasure() bool {
+	return 0 == rand.Intn(10)
+}
+
+func abdanddajda )_ ada
+
+
+// var missionCompleted bool
+// var numberOfGoroutines int
+
+// func main() {
+// 	var wg sync.WaitGroup
+// 	numberOfGoroutines = 10
+// 	wg.Add(numberOfGoroutines)
+
+// 	for i := 0; i < numberOfGoroutines; i++ {
+// 		go func() {
+// 			if foundTreasure() {
+// 				markMissionCompleted()
+// 			}
+// 			wg.Done()
+// 		}()
+// 	}
+// 	wg.Wait()
+
+// 	checkMissionCompletion()
+// }
+
+// func checkMissionCompletion() {
+// 	if missionCompleted {
+// 		fmt.Println("Mission is completed")
+// 	} else {
+// 		fmt.Println("Mission is failure")
+// 	}
+// }
+
+// func markMissionCompleted() {
+// 	missionCompleted = true
+// 	fmt.Println("Mission is now completed")
+// }
+
+// func foundTreasure() bool {
+// 	return 0 == rand.Intn(10)
+// }
